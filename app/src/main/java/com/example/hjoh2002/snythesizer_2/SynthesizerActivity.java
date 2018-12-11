24cefcecf38d1eaf081867d8fe0a82e61eff2d50
Hannah Johnson <hannah135johnson@yahoo.com>
2018-12-11 13:26:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/hjoh2002/snythesizer_2/SynthesizerActivity.java
@@ -0,0 +1,13 @@
+package com.example.hjoh2002.snythesizer_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SynthesizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synthesizer);
+    }
+}
