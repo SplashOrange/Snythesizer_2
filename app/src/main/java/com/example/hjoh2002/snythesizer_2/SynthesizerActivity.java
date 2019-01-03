60d0d14a4f7da43a6b2054b0a441d1858883c7cc
Hannah Johnson <hannah135johnson@yahoo.com>
2019-01-03 14:11:59 -0600

    Finished all of 1.2.4, however I don't have the sounds folder so I couldn't link it to the "raw" folder. I think that's why the word 'raw' in lines 29-31 in SynthesizerActivity.java have errors, but I'm not sure.

--- app/src/main/java/com/example/hjoh2002/snythesizer_2/SynthesizerActivity.java
+++ app/src/main/java/com/example/hjoh2002/snythesizer_2/SynthesizerActivity.java
@@ -1,13 +1,54 @@
 package com.example.hjoh2002.snythesizer_2;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SynthesizerActivity extends AppCompatActivity {
+    private static final String TAG =
+            SynthesizerActivity.class.getName();
+    private Button mEButton;
+    private Button mFButton;
+    private Button mXButton;
+    private MediaPlayer mpE;
+    private MediaPlayer mpF;
+    private MediaPlayer mpX;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
+        mEButton = (Button)findViewById(R.id.eButton);
+        mFButton = (Button)findViewById(R.id.fButton);
+        mXButton = (Button)findViewById(R.id.xButton);
+        mpE = MediaPlayer.create(this, R.raw.scalee);
+        mpF = MediaPlayer.create(this, R.raw.scalef);
+        mpX = MediaPlayer.create(this, R.raw.scalex);
     }
+
+    public void onButton1Click(View v) {
+        mpE.seekTo(0);
+        Log.e("SynthesizerActivity", "Button E clicked");
+        mpE.start();
+    }
+
+    public void onButton2Click(View v) {
+        mpF.seekTo(0);
+        Log.e("SynthesizerActivity", "Button F clicked");
+        mpF.start();
+    }
+
+    public void onButton3Click(View v) {
+        mpX.seekTo(0);
+        Log.e("SynthesizerActivity", "Button X clicked");
+        mpX.start();
+    }
+
+
 }
+
